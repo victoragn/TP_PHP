eb9be01bbf9c2102ded1d2c7c8390350a93c4f85
victoragn <victor.agnez@hotmail.fr>
2017-11-05 11:04:40 +0100

    ajout d'un fichier contenant les mots de passe et d'un .gitignore / 1ere initialisation de cible.php

---
+++ cible.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Vérification du MDP</title>
+    </head>
+    
+    <body>
+        <?php 
+            echo $_POST['mdp'];
+        ?>
+    </body>
+</html>
