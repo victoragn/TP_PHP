a3af843864ac9dcd488b939c8a43ec642ed90c81
victoragn <victor.agnez@hotmail.fr>
2017-11-05 10:52:58 +0100

    Ajout des fichiers index et cible et création du formulaire dans index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Entrez le MDP</title>
+    </head>
+    
+    <body>
+        <form action="cible.php" method="POST">
+            <label>Entrez le mot de passe : <input type="text" name="mdp"/></label>
+            <input type="submit" />
+        </form>
+    </body>
+</html>
